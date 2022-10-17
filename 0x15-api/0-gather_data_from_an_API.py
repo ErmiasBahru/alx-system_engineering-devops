9b93b654b44fe04ccef51b031f5dbae8481fb2ce
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-17 11:13:33 -0400

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests as r
+from sys import argv
+
+if __name__ == "__main__":
+    employee_id = argv[1]
+    resp_user = r.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    resp_todos = r.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
+
+    try:
+        users = resp_user.json()
+        user_todos = resp_todos.json()
+
+        completed_task = list(
+            filter(lambda x: x.get("completed") is True, user_todos))
+
+        print("Employee {} is done with tasks({}/{}):".format(users.get('name'),
+              len(completed_task), len(user_todos)))
+
+        for todo in completed_task:
+            print("\t {}".format(todo.get('title')))
+
+    except Exception as e:
+        print(e)
