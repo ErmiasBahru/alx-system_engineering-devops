0c619b2136c919a0b71e9293395bd807f3f8bf5d
Ermias Bahru <ermiasbahru878@gmail.com>
2022-10-18 01:28:53 -0400

    1. Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,17 +8,21 @@ import requests as r
 
 if __name__ == "__main__":
     employee_id = argv[1]
-    resp_user = r.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    resp_todos = r.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        employee_id)
+    url_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        employee_id)
+
+    resp_user = r.get(url_user)
+    resp_todos = r.get(url_todos)
 
     try:
         users = resp_user.json()
         user_todos = resp_todos.json()
         csv_name = "{}.csv".format(users.get('id'))
 
-        with open(csv_name, mode='w', encoding='utf-8', newline='') as csv_data:
+        with open(csv_name, mode='w', encoding='utf-8',
+                  newline='') as csv_data:
             csv_writer = csv.writer(csv_data, quoting=csv.QUOTE_ALL)
 
             for todo in user_todos:
